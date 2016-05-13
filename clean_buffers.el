6e5269cb4e341d565b50392f6258cf6456092add
ironjanowar <ironjanowar@gmail.com>
2016-05-13 02:41:30 +0200

    Fixed

--- clean_buffers.el
+++ clean_buffers.el
@@ -2,16 +2,18 @@
 ;;; Commentary:
 
 ;;; Code:
+
+(defun kill-buffers()
+  (let (buffer buffers)
+    (setq buffers (buffer-list))
+    (dotimes (i (length buffers))
+      (setq buffer (pop buffers))
+      (if (not (string-equal (buffer-name buffer) "*scratch*")) (kill-buffer buffer) (message "Not killing!")))))
+
 (defun clean-buffers() "Kills oll buffers except `scratch' and `Messages'"
        (interactive)
-       (yes-or-no-p "Do you really want to clean all buffers? ")
-       (let (buffer buffers)
-         (setq buffers (buffer-list))
-         (dotimes (i (length buffers))
-           (setq buffer (pop buffers))
-           (if (not (string-equal (buffer-name buffer) "*scratch*"))
-               (kill-buffer buffer)
-             (message "Not killing!")))))
+       (if (yes-or-no-p "Do you really want to clean all buffers? ")
+           (kill-buffers) nil))
 
 (provide 'clean_buffers)
 ;;; clean_buffers.el ends here
