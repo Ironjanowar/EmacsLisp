c4ac5dd704885283da2d6782c260a4f3d6a35af8
ironjanowar <ironjanowar@gmail.com>
2016-05-13 02:53:37 +0200

    Minor changes

--- clean_buffers.el
+++ clean_buffers.el
@@ -8,7 +8,7 @@
     (setq buffers (buffer-list))
     (dotimes (i (length buffers))
       (setq buffer (pop buffers))
-      (if (not (string-equal (buffer-name buffer) "*scratch*")) (kill-buffer buffer) (message "Not killing!")))))
+      (if (not (string-equal (buffer-name buffer) "*scratch*")) (kill-buffer buffer) nil))))
 
 (defun clean-buffers() "Kills oll buffers except `scratch' and `Messages'"
        (interactive)
